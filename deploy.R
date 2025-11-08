0451ab2c8db6dfb6f6b1f080f97a81a11b84e260
Mauricio Vargas Sepulveda <mavargas11@uc.cl>
2025-11-08 12:10:59 -0500

    add remotes

--- deploy.R
+++ deploy.R
@@ -1,3 +1,5 @@
 # Deploy
 install_github("rahulsh97/d3poindia", force = TRUE)
+install_github("pachadotdev/d3po", force = TRUE)
+install_github("pachadotdev/tabler", force = TRUE)
 rsconnect::deployApp(appFiles = "app.R")
